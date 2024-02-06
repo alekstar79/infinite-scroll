c1c83f5133c036d09c39010653700c7af5919a87
alekstar79 <alekstar79@yandex.ru>
2024-02-06 19:55:44 +0400

    header toolbar has been added

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,34 @@ import PageView from '@/views/PageView'
 
 <template>
   <v-layout>
+    <v-app-bar
+      color="primary"
+      height="64"
+      elevation="1"
+      prominent
+    >
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop=""
+      />
+      <v-toolbar-title>Infinite Scroll</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn
+        variant="text"
+        icon="mdi-magnify"
+      />
+      <v-btn
+        variant="text"
+        icon="mdi-filter"
+      />
+      <v-btn
+        variant="text"
+        icon="mdi-dots-vertical"
+      />
+    </v-app-bar>
+
     <v-main>
       <PageView />
     </v-main>
